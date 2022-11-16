b716e06eba484ce2f0e828d64f9eb488a005d7a5
Mikolaj Jaworski <72892088+xDemorn@users.noreply.github.com>
2022-11-16 10:36:34 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const ping = require('ping');
+const tcpp = require('tcp-ping');
 
 //Configuraciones
 // app.set('port', process.env.PORT || 3000);
@@ -11,7 +12,7 @@ var port = process.env.PORT || 5000;
 var host = process.env.HOST || 'localhost';
 
 //Middleware
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
@@ -23,21 +24,25 @@ app.use(express.json());
 //Nuestro primer WS Get
 app.get('/ping', (req, res) => {
 
-  //(async function () {
-    // const result = await ping.promise.probe(req.query.host, {
-    //   timeout: 10
-    // });
+  // (async function () {
+  // const result = await ping.promise.probe(req.query.host, {
+  //   timeout: 10
+  // });
 
-    ping.sys.probe(req.query.host, function (_alive, err) {
+  // ping.sys.probe(req.query.host, function (_alive, err) {
 
-      res.json({ 'alive': _alive, 'error': err });
-    }, {
-    timeout: 10,
-    // WARNING: -i 2 may not work in other platform like windows
-    extra: ['-i', '2'],
-});
-  
-  //})();
+  //   res.json({ 'alive': _alive, 'error': err });
+  // });
+
+  tcpp.probe('www.google.com', 80, function (err, available) {
+    res.send({ isAlive: available });
+  });
+
+  // tcpp.ping({ address: 'www.googles.com' }, function (err, data) {
+  //   res.send(data);
+  // });
+
+  // })();
 });
 
 //Iniciando el servidor
